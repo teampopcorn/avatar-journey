eef32fdacdd3a1399d2766436295e5176bccfe17
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-19 17:39:00 -0800

    Added icons to user-display and index.html

--- src/js-assets/user-display.js
+++ src/js-assets/user-display.js
@@ -6,9 +6,9 @@ function userDisplay(player) {
     userDisplaySection.appendChild(nameP);
     
     for(let i = 0; i < player.masteredElements.length; i++) {
-        const elementSpan = document.createElement('span');
-        elementSpan.textContent = player.masteredElements[i];
-        nameP.appendChild(elementSpan);
+        const elementIcon = document.createElement('img');
+        elementIcon.src = './assets/' + player.masteredElements[i] + '-icon.png';
+        nameP.appendChild(elementIcon);
     }
 }
 export default userDisplay;
