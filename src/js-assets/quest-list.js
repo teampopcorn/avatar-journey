648b33dac4b4c7e24ed93a33e10af22a20b58a7a
Tommy Tran <tranttommy95@gmail.com>
2019-02-21 11:12:02 -0800

    put in data for water quests

--- src/js-assets/quest-list.js
+++ src/js-assets/quest-list.js
@@ -5,52 +5,52 @@ const waterOne = {
         {
             choiceName: 'option 1',
             choiceDescription: 'You challenge your opponent to a rematch. They\'ve just disrespected the Avatar and that is unacceptable!',
-            choicePoints: 3,
+            choicePoints: 1,
             choiceResult: 'Master Tommy denies your request for a rematch at first, but grants it at your insistence and you were able to defeat them! The Avatar\'s honor was successfully defended!'
         },
         {
             choiceName: 'option 2',
             choiceDescription: 'You brush off the comment. You know that you\'ll master water bending soon enough.',
-            choicePoints: 1,
-            choiceResult: 'Master Tommy denies your request for a rematch at first, but grants it at your insistence and you were able to defeat them! The Avatar\'s honor was successfully defended! did bad'
+            choicePoints: 3,
+            choiceResult: 'Your opponent seems to have gotten riled up at your disinterest in their comment. They end up losing in the final round. Master Tommy praises you for your restraint.'
         }
     ]
 };
 
 const waterTwo = {
-    questTitle: 'water two',
-    questDescription: 'You try to bend water...Again',
+    questTitle: 'Ambush',
+    questDescription: 'On the way back to your igloo from water bending training, your are suddenly surrounded by a group of masked water benders. They all begin to attack you with projectile balls of ice.',
     questChoices: [
         {
             choiceName: 'option 1',
-            choiceDescription: 'Good',
-            choicePoints: 3,
-            choiceResult: 'You did good, once more'
+            choiceDescription: 'You stop the ice balls and launch a counter attack on each assailant.',
+            choicePoints: 1,
+            choiceResult: 'The masked water benders are all on the ground, having been subdued by you. Master Tommy appears and says "You still have much to learn before you can be a true water bender."'
         },
         {
             choiceName: 'option 2',
-            choiceDescription: 'Bad',
-            choicePoints: 1,
-            choiceResult: 'You did bad....still'
+            choiceDescription: 'You divert the path of the ice balls and have them hit your attackers.',
+            choicePoints: 3,
+            choiceResult: 'The masked water benders are all on the ground, having been subdued by you. Master Tommy appears and says "You\'re thinking like a true water bender!"'
         }
     ]
 };
 
 const waterThree = {
-    questTitle: 'water three',
-    questDescription: 'You bend water?',
+    questTitle: 'Kiyoshi on Fire',
+    questDescription: 'Master Tommy has given you the week off and so you decide to visit the island of Kiyoshi. You thought it would be a good opportunity to learn about your past life and maybe find guidance or wisdom on how to be a good Avatar. As Kiyoshi Island comes into view, you notice that smoke is starting to come up from the from the village. A lot of smoke, more than just from a simple fire. Someone has set the village aflame! As you get closer to the island, you see a small group of people fleeing the island and lighting the path behind them on fire. They board a small boat and are making their escape.',
     questChoices: [
         {
             choiceName: 'option 1',
-            choiceDescription: 'Good',
-            choicePoints: 3,
-            choiceResult: 'Yes'
+            choiceDescription: 'You rush to the village and try to put out the fires. Even though you\'re not a water bending master yet, you know that you can still help out!',
+            choicePoints: 1,
+            choiceResult: 'You were able to help the villagers put out the fire. They give you their gratitude and provide you hospitality while you continue to help them rebuild the damaged parts of the village. The villagers also share with you their history of Avatar Kiyoshi. You return to the Southern Water Tribe after a great week!'
         },
         {
             choiceName: 'option 2',
-            choiceDescription: 'Bad',
+            choiceDescription: 'You divert course and chase after the arsonists. If you don\'t catch them now, then they\'ll never be brought to justice!',
             choicePoints: 1,
-            choiceResult: 'No'
+            choiceResult: 'After chasing them for awhile, you begin to realize that you won\'t be able to catch up to them. You give up the pursuit and return to Kiyoshi Island only to find that it has been almost completely burned down. You do your best to help rebuild the village as much as you can, but the week ends and you have to leave. '
         }
     ]
 };
