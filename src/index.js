8a6723f20b87175c229ea88b334351ee6ca61638
heather <unphurlng@gmail.com>
2019-02-21 13:19:07 -0800

    index change for fire quest 1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ playerForm.addEventListener('submit', function(event) {
         incompleteQuests: {
             waterQuests: ['Sparring Tournament', 'Ambush', 'Kiyoshi on Fire'],
             earthQuests: ['earth one', 'earth two', 'earth three'],
-            fireQuests: ['fire one', 'fire two', 'fire three'],
+            fireQuests: ['Introduction to Fire Bending', 'fire two', 'fire three'],
             airQuests: ['Bending Air', 'Stop Rising Rebellion Army', 'Confrontation with a Hurricane -- Ego']
         },
         element: makeCycle(playerFormData.get('element'))
